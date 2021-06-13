db43c70b66d55fb16f6c282e40a8c9a2f1ae1ebe
dnf <1310332521@qq.com>
2021-06-13 11:27:12 +0000

    add git.nix

---
+++ features/git.nix
@@ -0,0 +1,31 @@
+
+{ config, pkgs, ... }:
+
+{
+
+  programs = {
+    git = {
+      # package = pkgs.gitAndTools.gitFull;
+      enable = true;
+      userName = "dnf";
+      userEmail = "1310332521@qq.com";
+      aliases = {
+        ckt = "checkout";
+        cmt = "commit";
+        cma = "commit --amend";
+        s = "status";
+        st = "status";
+        b = "branch";
+        pl = "pull --rebase";
+        pu = "push";
+      };
+      ignores = [ "*~" "*.swp" ];
+      extraConfig = {
+        init.defaultBranch = "master";
+        core.editor = "vim";
+        #protocol.keybase.allow = "always";
+        credential.helper = "store --file ~/.git-credentials";
+        pull.rebase = "true";
+      };
+    };
+}
