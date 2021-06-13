0cddd06a1f10b72e63a2b3dd1e2463470661b79c
dnf <1310332521@qq.com>
2021-06-13 11:26:40 +0000

    add nix-features

---
+++ features/nix-features.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+
+{
+  nix = {
+    package = pkgs.nixUnstable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+   };
+  nixpkgs.config.allowUnfree = true;
+}
