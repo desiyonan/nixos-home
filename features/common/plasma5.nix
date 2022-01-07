80d4085911b33d625044db182e46a9e6ead5ef04
dnf <1310332521@qq.com>
2022-01-07 21:25:20 +0800

    feat:set plasmawayland

--- features/common/plasma5.nix
+++ features/common/plasma5.nix
@@ -7,7 +7,7 @@
       enable = true;
       displayManager.sddm.enable = true;
       desktopManager.plasma5.enable = true;
-      # displayManager.defaultSession = "plasmawayland";
+      displayManager.defaultSession = "plasmawayland";
     };
   };
 }
