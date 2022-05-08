56cdea4e7aa88b48a693f6e9e42ce6e934aeeeb1
desiyonan <1310332521@qq.com>
2022-05-08 15:58:22 +0800

    feat:display update

--- features/common/display.nix
+++ features/common/display.nix
@@ -1,14 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  programs.xwayland.enable = true;
+  # programs.xwayland.enable = true;
   programs.qt5ct.enable = true;
   services = {
     xserver = {
       enable = true;
       desktopManager = {
         plasma5.enable = true;
-        plasma5.runUsingSystemd = true;
+        # plasma5.runUsingSystemd = true;
       };
       displayManager = {
         gdm.enable = true;
@@ -20,16 +20,16 @@
 
   environment = {
     systemPackages = with pkgs; [
-      mesa
-      weston
-      libdrm
-      libinput
-      wayland
-      xwayland
-      egl-wayland
-      wayland-protocols
-      libsForQt5.plasma-wayland-protocols
-      libsForQt5.qt5.qtwayland
+      # mesa
+      # weston
+      # libdrm
+      # libinput
+      # wayland
+      # xwayland
+      # egl-wayland
+      # wayland-protocols
+      # libsForQt5.plasma-wayland-protocols
+      # libsForQt5.qt5.qtwayland
     ];
     variables = {
       MOZ_ENABLE_WAYLAND="1";
