09ddf58101bcacac912cf411a6da926db776524a
Deng Fan <1310332521@qq.com>
2023-03-10 22:59:15 +0800

    feat: update pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,10 +2,10 @@
 with pkgs;
 {
   pkgs = pkgs;
-  idea-ultimate = pkgs.callPackage ./idea-ultimate.nix {};
+  idea-ultimate = pkgs.callPackage ./idea-ultimate {};
   latte-dock =   pkgs.libsForQt5.callPackage  ./latte-dock { inherit dotfiles;};
   # fcitx5-qt = pkgs.libsForQt5.callPackage ./fcitx5/fcitx5-qt.nix {};
-  # qv2ray-full = import ./v2ray.nix {inherit pkgs lib dotfiles;};
-  nvidia-offload = import ./nvidia-offload.nix {inherit pkgs;};
+  # qv2ray-full = import ./v2ray {inherit pkgs lib dotfiles;};
+  nvidia-offload = import ./nvidia-offload {inherit pkgs;};
 } 
 # // nixos-cn.legacyPackages.${system}
