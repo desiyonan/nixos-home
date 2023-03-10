d800ded03a4766384bd5e26776c2d28af1440c42
Deng Fan <1310332521@qq.com>
2023-03-11 00:31:01 +0800

    feat: nvidia-offload

--- modules/nvidia-offload.nix
+++ modules/nvidia-offload.nix
@@ -34,7 +34,17 @@ in {
     };
 
     environment.systemPackages = with mpkgs; [
-      nvidia-offload
+      # nvidia-offload
+      (
+        pkgs.writeShellScriptBin "nvidia-offload" 
+        ''
+        export __NV_PRIME_RENDER_OFFLOAD=1
+        export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+        export __GLX_VENDOR_LIBRARY_NAME=nvidia
+        export __VK_LAYER_NV_optimus=NVIDIA_only
+        exec -a "$0" "$@"
+        ''
+      )
     ];
 
     services.xserver.videoDrivers = [ "nvidia" ];
