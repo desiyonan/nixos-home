e53bb6c792ca04923447808e3068255be34b9a4a
dengfan <1310332521@qq.com>
2024-08-08 11:30:41 +0800

    feat: secrets for user

--- modules/features/secrets/default.nix
+++ modules/features/secrets/default.nix
@@ -1,48 +1,73 @@
-{config, lib, ...}:
+{config, lib, pkgs, ...}@args:
 let
+  helper = import ./secrets-for-user.nix args;
+
   host = config.networking.hostName;
   all_users = config.users.users;
-  # group = "users";
+  # group = users;
   ug = config.users.groups.users;
   # members
   users = lib.attrsets.mapAttrsToList (n: u: u) (lib.attrsets.filterAttrs (n: u: builtins.elem n ug.members) all_users);
-  mkUserSecretsMount = u:
-  let
-    uid = builtins.toString u.uid;
-    gid = builtins.toString ug.gid;
-    n = u.name;
-  in
-    [
-      "R  /run/user/${uid}/secrets - - - -"
-      "D  /run/user/${uid}/secrets 700 ${uid} ${gid} - -"
-      "C+ /run/user/${uid}/secrets/ssh - - - - /run/secrets/hosts/${host}/ssh"
-      # "C+ /run/user/${uid}/secrets/ssh/ssh_config - - - - /run/secrets/hosts/ssh_config"
-      "Z  /run/user/${uid}/secrets/* 400 ${uid} ${gid} - -"
 
-      "R  /home/${n}/.ssh - - - -"
-      "C+ /home/${n}/.ssh - - - - /run/user/${uid}/secrets/ssh"
-      "Z  /home/${n}/.ssh/* 600 ${uid} ${gid} - -"
-      "z  /home/${n}/.ssh/*.pub 644 ${uid} ${gid} - -"
-      "z  /home/${n}/.ssh 700 ${uid} ${gid} - -"
-    ]
+  host_clean_conf = pkgs.writeTextFile {
+    name = "nixos-tmpfiles.d";
+    destination = "/lib/tmpfiles.d/00-host-${host}.conf";
+    text = ''
+      # This file is created automatically and should not be modified.
+
+      ${lib.strings.concatStringsSep "\n" (helper.boot.buildCleanRules ug users)}
+    '';
+  };
+  u = all_users.dnf;
+  mount_confs = pkgs.writeTextFile {
+    name = "nixos-tmpfiles.d";
+    destination = "/lib/tmpfiles.d/users/00-${builtins.toString u.uid}.conf";
+    text = ''
+      # This file is created automatically and should not be modified.
+
+      ${lib.strings.concatStringsSep "\n" (helper.user.buildMountRules ug u)}
+    '';
+  };
+  services_config  = {
+    "secrets-tmpfiles-setup@" = {
+      enable = true;
+      restartIfChanged = false;
+      wantedBy = [
+        "user-runtime-dir@%i.service"
+      ];
+      after = [ "user-runtime-dir@%i.service" ];
+      before = [ "nixos-activation.service" ];
+      unitConfig.DefaultDependencies = "no";
+
+      serviceConfig = {
+        Type = "oneshot";
+
+        ExecStartPre=[
+          "/bin/sh -c 'systemctl set-environment u=$(${pkgs.getent}/bin/getent passwd %i | cut -d: -f1)'"
+          "/bin/sh -c 'cp ${mount_confs}/lib/tmpfiles.d/users/00-%i.conf /run/tmpfiles.d/00-user-%i.conf'"
+        ];
+        ExecStart = [
+          "/bin/sh -c 'echo user=$u'"
+          "systemd-tmpfiles --create --remove --clean --exclude-prefix=/dev --prefix=/run/user/%i --prefix=/home/dnf"
+        ];
+        ExecStartPost = [
+          "/bin/sh -c 'rm -rf /run/tmpfiles.d/00-user-%i.conf'"
+        ];
+      };
+    };
+  } // (builtins.listToAttrs (map (u: {
+    name = "secrets-tmpfiles-setup@${builtins.toString u.uid}";
+    value = {
+      enable = true;
+      wantedBy = [ "multi-user.target" ];
+      overrideStrategy = "asDropin";
+    };
+  }) users))
   ;
-  mkUsersSecretsMount = us: lib.lists.flatten (builtins.map mkUserSecretsMount us);
 in
 {
-  systemd.tmpfiles.rules = mkUsersSecretsMount users ++
-  [
-    # "R! /run/user/1000/secrets - - - -"
-    # "D  /run/user/1000/secrets 700 dnf users -"
-    # "L+ /home/dnf/.ssh/ssh_config - - - - /run/secrets/hosts/ssh_config"
-    # "D  /run/user/1000/secrets 744  0 - -"
-    # "D  /run/user/1000/secrets/ssh 744 1000 0 - -"
-    # "C+ /run/user/1000/secrets/ssh/s1_config - - - - /run/secrets/hosts/ssh_config"
-    # "Z  /run/user/1000/secrets 600 1000 100 - -"
-    # "z  /run/user/1000/secrets 700 1000 100 - -"
-
-    # "C+ /home/dnf/.ssh/s1_config - - - - /run/secrets/hosts/ssh_config"
-    # "Z  /home/dnf/.ssh 600 1000 100 - -"
-    # "z  /home/dnf/.ssh/*.pub 644 1000 100 - -"
-    # "z  /home/dnf/.ssh 700 1000 100 - -"
+  systemd.tmpfiles.packages = [
+    host_clean_conf
   ];
+  systemd.services = services_config;
 }
---
+++ modules/features/secrets/secrets-for-user.nix
@@ -0,0 +1,89 @@
+{config, lib, pkgs, ...}:
+with builtins;
+let
+  callTmpFilesRules = f: g: us:
+    let
+      users = lib.lists.toList us;
+    in
+    lib.lists.flatten (
+      lib.lists.forEach users (u:
+        let
+          p = {
+            n = u.name;
+            uid = toString u.uid;
+            gid = toString g.gid;
+            inherit g u;
+          };
+        in f p
+      )
+    )
+  ;
+
+in rec {
+  user = rec {
+    runtime = {
+      buildCleanRules = callTmpFilesRules (
+        {n, uid, gid, ...} : [
+          "R! /run/user/${uid}/secrets 700 ${uid} ${gid} - -"
+        ]
+      );
+
+      buildMountRules = callTmpFilesRules (
+        {n, uid, gid, ...} : [
+            # "e  /run/user/${uid}/secrets - - - 0"
+          "R  /run/user/${uid}/secrets 700 ${uid} ${gid} 0 -"
+          # "d  /run/user/${uid}/secrets 700 ${uid} ${gid} - -"
+          "C+ /run/user/${uid}/secrets/ - - - - /run/secrets/hosts/%l"
+          "Z  /run/user/${uid}/secrets/ 400 ${uid} ${gid} - -"
+        ]
+      );
+    };
+    program = rec {
+      ssh = {
+        buildCleanRules = callTmpFilesRules (
+          {n, uid, gid, ...} : [
+            "R! /home/${n}/.ssh 700 ${uid} ${gid} - -"
+          ]
+        );
+        buildMountRules = callTmpFilesRules (
+          {n, uid, gid, ...} : [
+            "R  /home/${n}/.ssh 700 ${uid} ${gid} - -"
+            "C+ /home/${n}/.ssh - - - - /run/secrets/hosts/%l/ssh"
+            "Z  /home/${n}/.ssh 600 ${uid} ${gid} - -"
+            "z  /home/${n}/.ssh/*.pub 644 ${uid} ${gid} - -"
+            "z  /home/${n}/.ssh 700 ${uid} ${gid} - -"
+          ]
+        );
+      };
+      buildCleanRules = g: u: (
+        ssh.buildCleanRules g u
+      );
+
+      buildMountRules = g: u: (
+        ssh.buildMountRules g u
+      );
+    };
+
+    buildCleanRules = g: u: (
+      program.buildCleanRules g u ++
+      runtime.buildCleanRules g u
+    );
+
+    buildMountRules = g: u: (
+      runtime.buildMountRules g u ++
+      program.buildMountRules g u
+    );
+  };
+
+  boot = {
+    buildCleanRules = g: u: (
+      user.buildCleanRules g u
+    );
+
+    buildMountRules = g: u: (
+      # callTmpFilesRules g u ({n, uid, gid, ...} : [])
+      []
+    );
+  };
+
+}
