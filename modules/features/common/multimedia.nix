8462b6da54cad78603a56a9e66fd14f7893e46aa
dengfan <1310332521@qq.com>
2024-09-03 16:27:13 +0800

    feat: fix pipewire

--- modules/features/common/multimedia.nix
+++ modules/features/common/multimedia.nix
@@ -3,21 +3,21 @@
 {
   sound.enable = false;
 
-  hardware = {
-    pulseaudio.enable = false;
-  };
-  
+  # hardware = {
+  #   pulseaudio.enable = true;
+  # };
+
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    wireplumber.enable= true;
-    audio.enable = true;
+    # wireplumber.enable= true;
+    # audio.enable = true;
     pulse.enable = true;
     alsa = {
       enable = true;
       support32Bit = true;
     };
-    jack.enable = true;
+    # jack.enable = true;
   };
 
 }
