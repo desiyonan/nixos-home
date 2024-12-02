3f2e1e35a5b3e23fcb5feaa1020d4482c31e1a63
deng fan <1310332521@qq.com>
2024-12-02 11:38:41 +0800

    feat: docker sevice

--- modules/services/dockerd/default.nix
+++ modules/services/dockerd/default.nix
@@ -20,17 +20,28 @@ in {
     systemd.services.dockerd = {
       enable = true;
       description = "Docker daemon.";
-      script = "${pkgs.docker}/bin/dockerd";
+      unitConfig = {
+        StartLimitBurst=3;
+        StartLimitInterval="60s";
+      };
       serviceConfig = {
-        Restart = "always";
+        Type="notify";
+        Restart = "on-failure";
+        ExecStart = "${pkgs.docker}/bin/dockerd";
+        ExecReload="/bin/kill -s HUP $MAINPID";
+        Delegate="yes";
         RestartSec = 5;
+        KillMode="process";
       };
       wants = [
-        "multi-user.target"
         "network-online.target"
       ];
       after = [
         "network-online.target"
+        "firewall.service"
+      ];
+      wantedBy = [
+        "multi-user.target"
       ];
       environment = {
         ALL_PROXY = "http://127.0.0.1:7890";
