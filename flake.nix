fd50e747ceac3c80fe6b49488db1ada4ef3d5669
邓帆 <1310332521@qq.com>
2026-04-15 20:50:15 +0800

    feat: udpate

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # https://status.nixos.org/
     # the current pinned base version
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11"; #24.11
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11"; #24.11
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     nixos-cn = {
